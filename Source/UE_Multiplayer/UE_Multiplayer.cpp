a81fa501bcf77c4ce11fe4cc6bf31f38c9394b9d
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2022-01-10 00:25:30 +0500

    Init

---
+++ Source/UE_Multiplayer/UE_Multiplayer.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Multiplayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Multiplayer, "UE_Multiplayer" );
+ 
